cc330cb4dfd24e4deeb04e54a5546eee849ff3f2
Paul Aderoju <paul.adeboye@yahoo.com>
2023-09-02 09:35:18 +0100

    Middleware + MongoDB 1

--- lesson4/app.js
+++ lesson4/app.js
@@ -1,25 +1,54 @@
 const express = require('express');
+const morgan = require('morgan');
 
 // express app
 const app = express();
 
+// Connect to MongoDB
+const dbUri = 'mongodb+srv://paul:paul1234@nodetuts.wcrvrdb.mongodb.net/?retryWrites=true&w=majority';
+
+// Register view engine
+app.set('view engine', 'ejs');
+
 // listen for requests
 app.listen(3000);
 
+// Middleware and static files
+app.use(express.static('public'))
+
+// Middleware are functions that run on the server. They run synchronously.
+app.use(morgan('dev'));
+
 app.get('/', (req, res) => {
-    res.sendFile('./index.html', { root: __dirname });
+    // res.sendFile('./views/index.html', { root: __dirname });
+    const blogs = [
+        {
+            title: 'CSS Variables',
+            snippet: 'CSS Variables are a great way to avoid redundant class names.'
+        },
+        {
+            title: 'AWS Global Infrastructure',
+            snippet: 'AWS Global Infrastructure is all about the various data centers and edge locations for hosting cloud apps.'
+        },
+    ];
+    res.render('index', {title: 'Home', blogs});
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile('./about.html', {root: __dirname})
+    res.render('about', {title: "About"});
 });
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create', {title: 'Create a new blog'});
+})
 
 // Redirects
 app.get('/about-us', (req, res) => {
-    res.redirect('/about')
+    res.redirect('./views/about')
 })
 
 // 404 Page . Always use this at the end of the code
 app.use((req, res) => {
-    res.status(404).sendFile('./404.html', {root: __dirname});
+    res.status(404).render('404', {title: '404 - Not Found'});
 });
+
